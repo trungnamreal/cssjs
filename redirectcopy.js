1084f648cc493e0fe4a757dd18a1bc70cfe42e95
trungnamreal <49482066+trungnamreal@users.noreply.github.com>
2019-04-10 20:25:22 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/Diaoctrungnam/'>http://https://www.facebook.com/Diaoctrungnam</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://www.facebook.com/Diaoctrungnam/'>https://www.facebook.com/Diaoctrungnam</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
